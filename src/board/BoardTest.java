aa5f317cc6e1bd1def6449a2f470ffa43a3615cb
Cristian Abrante <alu0100945850@ull.edu.es>
2018-03-03 11:35:49 +0000

    Board tests failed

--- src/board/BoardTest.java
+++ src/board/BoardTest.java
@@ -6,14 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class BoardTest {
-
+	
+	Board emptyTestBoard;
+	Board fileTestBoard;
+	
 	@Before
 	public void setUp() throws Exception {
+		emptyTestBoard = new Board();
+		fileTestBoard = new Board("src/test/board1.txt");
 	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
 }
